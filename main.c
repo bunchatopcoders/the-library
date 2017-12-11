4e1302705c3f489f541850dacdea58eeedcfe6b1
Hagar Usama <34040335+Hagar-Usama@users.noreply.github.com>
2017-12-11 15:49:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -25,6 +25,60 @@ void menu_reg (void);
 void menu_mem(void);
 void menu_search(void);
 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
+#define BOOKTILTE 50
+#define NAME 25
+
+typedef struct date {
+
+int day, mounth, year;
+
+
+} date;
+
+
+typedef struct book {
+    char title[BOOKTILTE];
+    char author[NAME];
+    int ISBN;
+    char publisher[NAME];
+    char category[NAME];
+    int availability;
+    //bool isSafe; // to check whether content is safe for underage
+    date date_pub;
+    //borrow -> copies
+
+}book;
+
+typedef struct bbooks // borrow book
+{
+   
+    int id; // id tany 5als
+    int ISBN; // ISBN tany 5als
+    date date_due;
+    date date_issue;
+    //int book.ISBN;
+
+} bbooks;
+typedef struct  member {
+
+    char name[2][NAME];
+    int id;
+    char address[3][NAME];
+    int age;
+    char phone[12];
+    char email[30];
+}member;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
 int main()
 {
 
