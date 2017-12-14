f949900f903aee99fdf66274294bf22638decc1f
youyou-39 <34109109+youyou-39@users.noreply.github.com>
2017-12-14 21:20:05 +0200

    Update remove_linem.c

--- remove_linem.c
+++ remove_linem.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void remove_line(int line,char*ch)
-
-
+void remove_line(int line,char*ch)//ch is the file name
+	
 int main()
 { int line;
  printf("enter the line number: ");
