2c429bf50209b31532bcf8729d313dd0864506c1
youyou-39 <34109109+youyou-39@users.noreply.github.com>
2017-12-14 21:19:04 +0200

    Update remove_linem.c

--- remove_linem.c
+++ remove_linem.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void remove_line();
+void remove_line(int line,char*ch)
 
 
 int main()
-{ remove_line();
+{ int line;
+ printf("enter the line number: ");
+    scanf("%d",&line);
+	remove_line(line,"student_file.txt");
     return 0;
 }
 
-
-void remove_line()
-{   int line,i=1;
+void remove_line(int line,char*ch)
+{   int i=1;
    char c;
-    printf("enter the line number: ");
-    scanf("%d",&line);
     FILE*f1,*f2;
-  f1=fopen("student_file.txt","r");
+  f1=fopen(ch,"r");
   f2=fopen("student_file_remove.txt","w");
   if(f1!=NULL)
    {
 	   //!feof(f1)
-	  
+
        while( (c = fgetc(f1)) != EOF)
-    {  
-		
+    {
+
         if(i!=line)
-        fprintf(f2,"%c",c); 
+        fprintf(f2,"%c",c);
 
        if (c=='\n')
        i++;
     }
       fclose(f1);
       fclose(f2);
-      remove("student_file.txt");
-      rename("student_file_remove.txt","student_file.txt");
+      remove(ch);
+      rename("student_file_remove.txt",ch);
    }
    else perror("ERROR");
 
 }
-
